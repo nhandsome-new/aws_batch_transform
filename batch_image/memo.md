d76b2c5ac01f03aadf729723e97b028f68766c2b
nhandsome-new <hanbs044@gmail.com>
2022-08-23 02:52:27 +0900

    update

---
+++ batch_image/memo.md
@@ -0,0 +1,22 @@
+1. Setting1
+
+```python
+max_concurrent_transforms = None
+max_payload = None
+strategy='SingleRecord'
+split_type='Line'
+```
+
+[34m2022-08-22T16:39:16,433 [INFO ] W-9000-model_1.0-stdout MODEL_LOG - request received!![0m
+[35m2022-08-22T16:39:16,407 [INFO ] W-9001-model_1.0-stdout MODEL_LOG - 1[0m
+
+1. Setting2
+
+```python
+max_concurrent_transforms = None
+max_payload = None
+strategy='SingleRecord'
+split_type=None
+```
+
+[34m2022-08-22T16:50:38,008 [INFO ] W-9001-model_1.0-stdout MODEL_LOG - 2400[0m
--- batch_image/src/resnet_transform.py
+++ batch_image/src/resnet_transform.py
@@ -90,98 +90,6 @@ def _get_test_data_loader(test_batch_size, training_dir, **kwargs):
         batch_size=test_batch_size,
         shuffle=False, **kwargs)
 
-def _average_gradients(model):
-    # Gradient averaging.
-    size = float(dist.get_world_size())
-    for param in model.parameters():
-        dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM)
-        param.grad.data /= size
-        
-def train(args):
-    is_distributed = len(args.hosts) > 1 and args.backend is not None
-    logger.debug("Distributed training - {}".format(is_distributed))
-    use_cuda = args.num_gpus > 0
-    logger.debug("Number of gpus available - {}".format(args.num_gpus))
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    device = torch.device("cuda" if use_cuda else "cpu")
-
-    if is_distributed:
-        # Initialize the distributed environment.
-        world_size = len(args.hosts)
-        os.environ['WORLD_SIZE'] = str(world_size)
-        host_rank = args.hosts.index(args.current_host)
-        os.environ['RANK'] = str(host_rank)
-        dist.init_process_group(backend=args.backend, rank=host_rank, world_size=world_size)
-        logger.info('Initialized the distributed environment: \'{}\' backend on {} nodes. '.format(
-            args.backend, dist.get_world_size()) + 'Current host rank is {}. Number of gpus: {}'.format(
-            dist.get_rank(), args.num_gpus))
-
-    # set the seed for generating random numbers
-    torch.manual_seed(args.seed)
-    if use_cuda:
-        torch.cuda.manual_seed(args.seed)
-
-    train_loader = _get_train_data_loader(args.batch_size, args.data_dir, is_distributed, **kwargs)
-    # test_loader = _get_test_data_loader(args.test_batch_size, args.data_dir, **kwargs)
-
-    logger.debug("Processes {}/{} ({:.0f}%) of train data".format(
-        len(train_loader.sampler), len(train_loader.dataset),
-        100. * len(train_loader.sampler) / len(train_loader.dataset)
-    ))
-
-    # logger.debug("Processes {}/{} ({:.0f}%) of test data".format(
-    #     len(test_loader.sampler), len(test_loader.dataset),
-    #     100. * len(test_loader.sampler) / len(test_loader.dataset)
-    # ))
-
-    model = resnet20().to(device)
-    if is_distributed and use_cuda:
-        # multi-machine multi-gpu case
-        model = torch.nn.parallel.DistributedDataParallel(model)
-    else:
-        # single-machine multi-gpu case or single-machine or multi-machine cpu case
-        model = torch.nn.DataParallel(model)
-
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-
-    for epoch in range(1, args.epochs + 1):
-        model.train()
-        for batch_idx, (data, target) in enumerate(train_loader, 1):
-            data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.nll_loss(output, target)
-            loss.backward()
-            if is_distributed and not use_cuda:
-                # average gradients manually for multi-machine cpu case only
-                _average_gradients(model)
-            optimizer.step()
-            if batch_idx % args.log_interval == 0:
-                logger.info('Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.sampler),
-                    100. * batch_idx / len(train_loader), loss.item()))
-    # test(model, test_loader, device)
-    save_model(model, args.model_dir)
-
-
-def test(model, test_loader, device):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item()  # sum up batch loss
-            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-    logger.info('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
-
 def model_fn(model_dir):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = torch.nn.DataParallel(resnet20())
@@ -189,6 +97,32 @@ def model_fn(model_dir):
         model.load_state_dict(torch.load(f))
     return model.to(device)
 
+def input_fn(request_body, content_type='application/jsonlines'):
+    if content_type == 'application/jsonlines':
+        print("request received!!")
+        print(type(request_body))
+        print(f'requestbody:{request_body}')
+        # Warning: for some reason, when Sagemaker is doing batch transform,
+        # it automatically adds a line break in the end, needs to strip the line break to avoid errors.
+        # Sagemaker Endpoint doesn't have such issue.
+        lines = request_body.decode("utf-8").rstrip(os.linesep).split(os.linesep)
+        data = []
+        for line in lines:
+            line = line.strip()
+            input_data = json.loads(line)
+            print(input_data)
+            data.append(input_data)
+        print(len(data))
+        return data
+
+    raise Exception(f'Requested unsupported ContentType in content_type {content_type}')
+
+def predict_fn(input_obj, model):
+    logger.debug(input_obj)
+    return {"predictions": input_obj}
+
+def output_fn(predictions, response_content_type):
+    return json.dumps(predictions)
 
 def save_model(model, model_dir):
     logger.info("Saving the model.")
